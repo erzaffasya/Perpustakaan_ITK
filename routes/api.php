7893d56c1a603488edcfe4280bbdfa6d24b6c43d
erza <58721335+erzaffasya@users.noreply.github.com>
2022-06-06 09:18:54 +0800

    revisi

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,9 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::get('/', function () {
+    return abort(403, 'Dokumentasi ada di GRUP.');
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return abort(403, 'Unauthorized action.');
+    return abort(403, 'Sabar yak.');
 });
 
 // Route::get('/dashboard', function () {
