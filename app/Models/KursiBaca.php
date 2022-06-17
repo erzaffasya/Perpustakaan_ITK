f04c1f4642d312686035c774c4a2c1d8bc8d1bec
erza <effasya@gmail.com>
2022-06-17 10:42:46 +0800

    RuanganBaca, LetakBaca,Peminjaman -mscf

---
+++ app/Models/KursiBaca.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class KursiBaca extends Model
+{
+    use HasFactory;
+}
